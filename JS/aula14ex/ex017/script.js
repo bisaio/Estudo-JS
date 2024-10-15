1ca8940c10706afbb70e0a9e680c865513c95df5
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2024-10-15 20:02:08 -0300

    ex17 - gerar tabuada

--- JS/aula14ex/ex017/script.js
+++ JS/aula14ex/ex017/script.js
@@ -1,1 +1,40 @@
-// alert('Olá')
+class Elementos {
+    /** @return {HTMLInputElement} */
+    static get inputNumero() {
+        return document.querySelector("#numero")
+    }
+
+    /** @return {HTMLButtonElement} */
+    static get buttonGerar() {
+        return document.querySelector("#gerar")
+    }
+
+    /** @return {HTMLSelectElement} */
+    static get selectTabuada() {
+        return document.querySelector("#tabuada")
+    }
+}
+
+Elementos.buttonGerar.addEventListener('click', gerarTabuada)
+
+let item = document.createElement('option')
+item.text = 'Escolha um numero'
+Elementos.selectTabuada.appendChild(item)
+
+function gerarTabuada() { 
+    let numero = Number(Elementos.inputNumero.value)
+
+    if (Elementos.inputNumero.value === ""){
+        alert("preencha o campo de número")
+    } else {
+        Elementos.selectTabuada.innerHTML = ""
+
+        for(i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${numero} x ${i} = ${numero * i}`
+            Elementos.selectTabuada.appendChild(item)
+        }
+    }
+
+
+}
