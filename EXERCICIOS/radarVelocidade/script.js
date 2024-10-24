f208eaed599d96200cd5ef3fd9ebc4414e63aaa1
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2024-10-23 22:18:37 -0300

    WIP: ex - inicio do radar

--- EXERCICIOS/radarVelocidade/script.js
+++ EXERCICIOS/radarVelocidade/script.js
@@ -1,1 +1,5 @@
-// alert('Olá')
+//FIXME - fazer com a tolerancia real
+/*
+abaixo de 100, a velocidade dectada é considerada como x - 7
+acima de 100, a velocidade é considerada como x + 7% de x
+*/
