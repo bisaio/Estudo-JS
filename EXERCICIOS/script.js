f49b75abbe571e4f74c4f6f30f83a792978c1053
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2024-10-29 21:02:27 -0300

    HOTFIX: index
    mudando a inserção manual de links para as atividades diretamente no html para uma lista no JS que utiliza de um forEach para inserir os cards das atividades na tela

--- EXERCICIOS/script.js
+++ EXERCICIOS/script.js
@@ -1,1 +1,24 @@
-// alert('Olá')
+/** 
+* @typedef Atividade
+* @property {string} link - link de direcionamento para a atividade
+* @property {string} nome - nome da atividade
+*/
+
+/** @type {Atividade[]} */
+const atividades = [
+    {link: 'radarVelocidade/radarVelocidade.html', nome: 'Radar de Velocidade'},
+    {link: 'fibonacci/fibonacci.html', nome: 'Fibonacci'},
+    {link: 'fatorial/fatorial.html', nome: 'Fatorial'},
+    {link: 'randomNumber/randomNumber.html', nome: 'Número aleatório'}
+]
+
+const atvs = document.querySelector("#atvs")
+
+atividades.forEach(atividade => {
+   atvs.innerHTML +=
+    `
+    <a href='${atividade.link}'>
+        <section> ${atividade.nome} </section>
+    </a>
+   ` 
+});
