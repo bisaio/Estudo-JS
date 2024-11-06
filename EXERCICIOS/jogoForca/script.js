05d0f7a2f36b501e423207d81af23d8bcd95acad
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2024-11-06 18:57:52 -0300

    WIP: ex - forca

--- EXERCICIOS/jogoForca/script.js
+++ EXERCICIOS/jogoForca/script.js
@@ -1,7 +1,17 @@
 const letra = document.querySelector("#letra")
 const res = document.querySelector("#res")
-const confirmar = document.querySelector("#confirmar")
+const confirm_word = document.querySelector("#confirm_word")
+const confirm_letter = document.querySelector("#confirm_letter")
+const word = document.querySelector("#div_word")
+const letter = document.querySelector("#div_letter")
+
+confirm_word.addEventListener("click", forca)
 
 function forca() {
-    
+    word.style.display = 'none'
+    letter.style.display = 'block'
+
+    res.innerHTML = `FORCA`
 }
+
+//FIXME - implementar a possibilidade de escolher entre inserir a palavra ou colocar palavras pre-selecionadas
